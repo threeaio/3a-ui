00c9218be98757cda32bcaa63245b1e4f8ef6ad5
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2025-03-31 08:44:04 +0200

    dashboard WIP

--- apps/docs/app/features/data-dashboard/page.tsx
+++ apps/docs/app/features/data-dashboard/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Metrics } from './components/widgets/top-metrics/metrics'
-
+import { TaskStatusWidget } from './components/widgets/task-status-bar/task-status-widget'
 export default function DataDashboardPage() {
   return (
     <main className="flex-1 bg-background">
       <div className="flex flex-col h-full p-5">
         <Metrics />
+        <TaskStatusWidget className="h-40" />
       </div>
     </main>
   )
