309591b0a11aed87610ffe1f0f628792664bd46d
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2025-04-01 22:13:07 +0200

    dashboard WIP layout WIP for tasks page

--- packages/ui/src/ui/card/card.tsx
+++ packages/ui/src/ui/card/card.tsx
@@ -17,7 +17,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
     <div
       data-slot="card-header"
       className={cn(
-        '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
+        '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-5 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-5',
         className,
       )}
       {...props}
@@ -26,7 +26,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
 }
 
 function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
-  return <div data-slot="card-title" className={cn('leading-none text-sm', className)} {...props} />
+  return <div data-slot="card-title" className={cn('leading-none text-sm font-normal', className)} {...props} />
 }
 
 function CardDescription({ className, ...props }: React.ComponentProps<'div'>) {
